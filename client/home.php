5edb9075982bcb416984fff6fd534c4ab1ecd07a
lucas-zillmer <lucas.zillmer@bitzsoftwares.com.br>
2022-11-24 15:36:57 -0300

    Added home page and fixed login

---
+++ client/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="img/icon.png"/>
+    <link rel="stylesheet" href="css/style.css"/>
+    <title>Bitz Barbearia</title>
+</head>
+
+<body>
+    <div class="wrapperLogin">
+        <div class="boxLogin">
+            <hr>aaa</hr>
+</body>
